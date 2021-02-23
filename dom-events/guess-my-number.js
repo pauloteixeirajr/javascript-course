0e9521ba3a11ac0177269f3761bf8de6af8d0483
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-02-23 19:53:50 +0000

    guess my number: implementing the game logic

--- dom-events/guess-my-number.js
+++ dom-events/guess-my-number.js
@@ -1,11 +1,34 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = +document.querySelector('.guess').value;
   const message = document.querySelector('.message');
+  const curScore = document.querySelector('.score');
 
   if (!guess) {
-    message.textContent = 'No number!';
-    return;
+    message.textContent = '🚫  No number!';
+  } else if (guess === secretNumber) {
+    message.textContent = '✅  Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      message.textContent = '📈 Too high!';
+      score--;
+      curScore.textContent = score;
+    } else {
+      message.textContent = 'You lost the game. :(';
+      curScore.textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      message.textContent = '📉 Too low!';
+      score--;
+      curScore.textContent = score;
+    } else {
+      message.textContent = 'You lost the game. :(';
+      curScore.textContent = 0;
+    }
   }
 });
