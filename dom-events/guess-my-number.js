0fa9f72a0be9f33c98a24df07b9f66f9c91038fd
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-02-23 19:31:02 +0000

    guess my number: handling click events

--- dom-events/guess-my-number.js
+++ dom-events/guess-my-number.js
@@ -1,1 +1,11 @@
 'use strict';
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = +document.querySelector('.guess').value;
+  const message = document.querySelector('.message');
+
+  if (!guess) {
+    message.textContent = 'No number!';
+    return;
+  }
+});
