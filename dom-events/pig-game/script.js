1d6455d24b9d2ff7c63faa803368d25ecb10aa3d
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-02-25 19:25:58 +0000

    pig game: rolling the dice

--- dom-events/pig-game/script.js
+++ dom-events/pig-game/script.js
@@ -1,1 +1,39 @@
 'use strict';
+
+// Score Variables
+let currentScore = 0;
+
+// Selecting elements
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display the dice
+  diceEl.src = `dice-${dice}.png`;
+  diceEl.classList.remove('hidden');
+
+  // 3. Check for rolled 1
+  if (dice !== 1) {
+    // Add dice to the current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // switch to next player
+  }
+});
