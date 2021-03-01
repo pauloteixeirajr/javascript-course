611eb0f9a8d0acf2fe8b20c6b74a7f63f56ebe4d
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-03-01 18:54:25 +0000

    scope & the scope chain

--- behind-the-scenes/script.js
+++ behind-the-scenes/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+// Scope & the Scope Chain
+function calcAge(birthYear) {
+  const age = new Date().getFullYear() - birthYear;
+  console.log(firstName);
+
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `Oh, and you are a millennial, ${firstName}`;
+      console.log(str);
+    }
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Paulo';
+calcAge(1989);
