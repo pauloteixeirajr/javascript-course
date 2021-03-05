db0c105645a6ee8c2fde727f47a2a230f7b3801d
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-03-05 14:00:00 +0000

    data structures: maps fundamentals

--- data-structures/script.js
+++ data-structures/script.js
@@ -329,3 +329,53 @@ for (const order of ordersSet) console.log(order);
 const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
 const staffUnique = [...new Set(staff)];
 console.log(staffUnique);
+
+// Maps (Data structure that we can map values to keys)
+// The big difference between maps and objects
+// is that in Maps, keys can have any type
+console.clear();
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+rest.set(2, 'Lisbon, Portugal');
+
+// You can chain set methods
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
+
+console.log(rest);
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+// Check if value is in Map
+console.log(rest.has('categories'));
+
+// Delete value from Map
+rest.delete(2);
+
+console.log(rest);
+
+// Check the size of map
+console.log(rest.size);
+
+// Clear the Map
+// rest.clear();
+
+// We can also use arrays or objects as map keys
+// but you must reference the same instance of
+// the same array or same object in the heap
+const oneTwo = [1, 2];
+rest.set(oneTwo, 'Test');
+console.log(rest.get(oneTwo));
+
+rest.set(document.querySelector('h1'), 'Heading');
