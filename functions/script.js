62b729e7c24612afe4b86b51722c858418209e98
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-03-09 21:11:10 +0000

    functions: value vs reference

--- functions/script.js
+++ functions/script.js
@@ -20,3 +20,32 @@ createBooking('LH123');
 createBooking('LH123', 3, 800);
 createBooking('LH123', 4);
 createBooking('LH123', undefined, 1000);
+
+// How passing arguments works: Value vs Reference
+const flightNumber = 'LH234';
+const passenger = {
+  name: 'Paulo T',
+  passport: 12345678910,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 12345678910) {
+    console.log('Check in');
+  } else {
+    console.log('Wrong passport!');
+  }
+};
+
+checkIn(flightNumber, passenger);
+console.log(flightNumber); // is still LH234
+console.log(passenger); // Name has changed
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+
+newPassport(passenger);
+checkIn(flightNumber, passenger);
