68119f5261939d745c3adfb23b0c26cc2da104f1
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-03-09 21:00:57 +0000

    functions: default parameters

--- functions/script.js
+++ functions/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// Default parameters
+const bookings = [];
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  bookings.push(booking);
+  console.log(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 3, 800);
+createBooking('LH123', 4);
+createBooking('LH123', undefined, 1000);
