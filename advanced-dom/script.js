a4ed71c2fd8b151c511e07e955ad18bafcb53e62
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-03-17 21:29:49 +0000

    advanced dom: building a tabbed component

--- advanced-dom/script.js
+++ advanced-dom/script.js
@@ -69,3 +69,26 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
+
+// Building a Tabbed Component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clickedTab = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clickedTab) return;
+
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(t => t.classList.remove('operations__content--active'));
+
+  // Activate tab
+  clickedTab.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clickedTab.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
