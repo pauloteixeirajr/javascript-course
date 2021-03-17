f9d174277c7520dea161fa29ffedd5d68fb5dc84
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-03-17 21:53:40 +0000

    advanced dom: the scroll event

--- advanced-dom/script.js
+++ advanced-dom/script.js
@@ -112,3 +112,11 @@ const handleHover = function (e) {
 
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+const initialCoords = section1.getBoundingClientRect();
+
+// Implementing a Sticky Navigation: The Scroll Event
+window.addEventListener('scroll', function () {
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
