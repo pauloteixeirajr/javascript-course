4f86649e3963c9a2ce8d4d43568f9abc981338f8
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-03-18 20:29:15 +0000

    oop: prototypal inheritance on builtin objects

--- oop/script.js
+++ oop/script.js
@@ -40,3 +40,22 @@ console.log(paulo.hasOwnProperty('species')); // false
 
 console.log(paulo.__proto__ === Person.prototype);
 console.log(Person.prototype.isPrototypeOf(paulo));
+
+// Prototypal Inheritance on Built-in Objects
+const arr = [3, 6, 4, 5, 6, 9, 3, 9, 3, 3];
+console.log(arr.__proto__);
+
+// Not a good a practice to extend built-in prototypes
+// - Next version might add a function with the same name
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+// DOM Elements
+const h1 = document.querySelector('h1');
+console.log(h1.__proto__);
+
+// Functions
+console.dir(x => x + 1);
