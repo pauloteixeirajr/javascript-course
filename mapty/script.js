ca04de9f810ddfc9839da1e1d6a6d1d016d9b22b
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-03-22 17:28:56 +0000

    mapty: using the geolocation API

--- mapty/script.js
+++ mapty/script.js
@@ -22,3 +22,16 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if ('geolocation' in navigator) {
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      const { latitude, longitude } = position.coords;
+      console.log(latitude, longitude);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    error => {
+      console.log('Cold not get current position', error);
+    }
+  );
+}
