e8d6de4d45a7c8e283a4fb959d456a7f1773214a
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-03-22 17:43:03 +0000

    mapty: displaying a map using leaflet library

--- mapty/script.js
+++ mapty/script.js
@@ -26,9 +26,21 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if ('geolocation' in navigator) {
   navigator.geolocation.getCurrentPosition(
     position => {
+      const tileLayer = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+      const footNote =
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
       const { latitude, longitude } = position.coords;
-      console.log(latitude, longitude);
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer(tileLayer, {
+        attribution: footNote,
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('You can add any labels in here')
+        .openPopup();
     },
     error => {
       console.log('Cold not get current position', error);
