9e0d29a93fe84720384b8e83f6b20545da9870de
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-03-23 18:45:08 +0000

    async: callback hell

--- asynchronous/script.js
+++ asynchronous/script.js
@@ -3,17 +3,9 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
-  request.send();
-
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-
-    const html = `
-      <article class="country">
+const renderCountry = function (data, className = '') {
+  const html = `
+      <article class="country ${className}">
         <img class="country__img" src="${data.flag}" />
         <div class="country__data">
           <h3 class="country__name">${data.name}</h3>
@@ -27,11 +19,38 @@ const getCountryData = function (country) {
       </article>
     `;
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryDataAndNeighbour = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render first country
+    renderCountry(data);
+
+    // Get neighbour country
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    request2.send();
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      // Render second country
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('portugal');
-getCountryData('br');
-getCountryData('ireland');
+getCountryDataAndNeighbour('ireland');
