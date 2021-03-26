c7d5f010cd0e8efd9b28ae219b57f529623ba9f4
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-03-26 20:25:05 +0000

    forkify: loading a recipe from API

--- forkify-app/src/js/controller.js
+++ forkify-app/src/js/controller.js
@@ -11,3 +11,31 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    const { recipe: values } = data.data;
+    const recipe = {
+      cookingTime: values.cooking_time,
+      id: values.id,
+      image: values.image_url,
+      ingredients: values.ingredients,
+      publisher: values.publisher,
+      servings: values.servings,
+      sourceUrl: values.source_url,
+      title: values.title,
+    };
+
+    console.log(recipe);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+showRecipe();
