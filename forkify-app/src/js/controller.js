c2211135b2a40a0700dca61431b2a7c391c954d7
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-03-30 20:45:15 +0100

    forkify: updating recipe servings

--- forkify-app/src/js/controller.js
+++ forkify-app/src/js/controller.js
@@ -42,8 +42,17 @@ const controlPagination = function (page) {
   paginationView.render(model.state.search);
 };
 
+const controlServings = function (servings) {
+  // update the recipe servings (in state)
+  model.updateServings(servings);
+
+  // update the recipe view
+  recipeView.render(model.state.recipe);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
